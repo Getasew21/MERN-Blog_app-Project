394e494ea0b7db6acec5f100e9b2a1d0feee6274
Dawithabte31 <dawithabitamu31@gmail.com>
2024-02-12 02:34:33 +0300

    navebar changes

--- blog-frontnd/src/components/Navbar.tsx
+++ blog-frontnd/src/components/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar() {
 </span>
         </div>
 
-        <ul className={`md:flex md:items-center  md:pb-0 pb-12 absolute md:static bg-gray-800 justify-center md:bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'opacity-100 top-20' : 'top-[-490px]'} md:opacity-100 opacity-100`}>
+        <ul className={`md:flex md:items-center md:mr-4  md:pb-0 pb-12 absolute md:static bg-gray-800 justify-center md:bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'opacity-100 top-20' : 'top-[-490px]'} md:opacity-100 opacity-100`}>
           <NavLink style={{ textDecoration: "none", color: "rgb(136, 134, 134)" }} to='/'><li className='md:ml-8 text-xl text-gray-100 md:text-gray-800 hover:text-gray-400 duration-500 md:my-0 my-7'>HOME</li></NavLink>
           <NavLink style={{ textDecoration: "none", color: "rgb(136, 134, 134)" }} to='/register'><li className='md:ml-8 text-xl text-gray-100 md:text-gray-800 hover:text-gray-400 duration-500 md:my-0 my-7'>REGISTER</li></NavLink>
           <NavLink style={{ textDecoration: "none", color: "rgb(136, 134, 134)" }} to='/write'><li className='md:ml-8 text-xl text-gray-100 md:text-gray-800 hover:text-gray-400 duration-500 md:my-0 my-7'>WRITE</li></NavLink>
@@ -37,7 +37,7 @@ function Navbar() {
           <NavLink to="/setting" key="profileimg" className="hidden md:flex  ">
             {user.profilePic && (
               <img
-                className='profileimage self-end object-cover h-10 w-10 rounded-full overflow-hidden bg-gray-500 '
+                className='profileimage self-end md:ml-8  object-cover h-10 w-10 rounded-full overflow-hidden bg-gray-500 '
                 src={PF + user.profilePic}
                 alt='img'
               />
